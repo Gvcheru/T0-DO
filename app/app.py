7604d8bbf9e4dd2932bb514b63b17557afe73c75
Gvcheru <159762060+Gvcheru@users.noreply.github.com>
2024-06-21 16:34:07 +0300

    a

--- app/app.py
+++ app/app.py
@@ -1,25 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-from models import db, Task
+from app import create_app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-db.init_app(app)
-
-@app.route('/')
-def index():
-    return render_template('templates/index.html')
-
-@app.route('/tasks', methods=['GET', 'POST'])
-def tasks():
-    if request.method == 'GET':
-        tasks = Task.query.all()
-        return jsonify([task.to_dict() for task in tasks])
-    elif request.method == 'POST':
-        data = request.get_json()
-        task = Task(title=data['title'], description=data['description'], date=data['date'])
-        db.session.add(task)
-        db.session.commit()
-        return jsonify(task.to_dict()), 201
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
